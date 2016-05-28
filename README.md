5736f26a5ea1803ae57181356f00803683ea0586
jquinchi <jquinchi@gmail.com>
2016-05-28 09:48:50 -0500

    Actualizacion del archivo readme

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # MVE - Magnificación de video euleriano
 
 Se pretende realizar una medición del ritmo cardiaco, mediante la implementación de la magnificación de video euleriano.
-*_Esta implementación se ha realizado en OpenCV 3.1_*
+**_Esta implementación se ha realizado en OpenCV 3.1_**
 ## Creditos
 * [Joseph Pan](https://github.com/wzpan/QtEVM.git): Por su contribución de aplicación en aplicación de EVM mediate QT. Tiene gran cantidad de opciones para la implementación de este método.
 * [Matt Hammond](https://github.com/mc-jesus/face_detect_n_track.git): Por su mejora de detección facial, bastante rápida en ejecución.
--- filtroTemporal/main.cpp
+++ filtroTemporal/main.cpp
@@ -17,6 +17,8 @@ int main()
     cin>> capa;
 
     VideoCapture video(0);
+    namedWindow("Resultado",WINDOW_AUTOSIZE);
+
     while(1){
         video >> img;
         img.convertTo(img,CV_32FC3,1.0/255.0f);
@@ -25,25 +27,16 @@ int main()
         pasabajo1=piramide;
         pasabajo2=piramide;
         filtrado = piramide;
+       hconcat(img,piramide[0],imgOut);
+
+
+        imshow("Resultado",imgOut);
         for(int i=0;i<capa;i++)
         {
 
-            //fltrTemporal(piramide[i],filtrado[i],i);
-            Mat temp1,temp2,temp3;
-            temp3 = piramide.at(i);
-            float fh = 0.4;
-            float fl= 0.05;
-            temp1 = (1-fh)*pasabajo1[i] + fh*temp3;
-            temp2 = (1-fl)*pasabajo2[i] + fl*temp3;
-            pasabajo1[i] = temp1;
-            pasabajo2[i] = temp2;
-            filtrado.at(i) = pasabajo1[i]-pasabajo2[i];
+            fltrTemporal(piramide[i],filtrado[i],i);
+
         }
-        hconcat(img,piramide[0],imgOut);
-
-
-        namedWindow("Resultado",WINDOW_AUTOSIZE);
-        imshow("Resultado",imgOut);
 
         if(waitKey(30)>=0) break;
     }
